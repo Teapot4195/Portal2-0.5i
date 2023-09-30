a132faf78472fcc62b481199c67e442e13adb2e9
Skye <79576345+lych3e@users.noreply.github.com>
2023-09-30 23:34:00 +0100

    Improved player
    Smoother movement & running

--- game/scripts/player.gd
+++ game/scripts/player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody3D
 
-var speed := 5;
+var speed;
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity");
 const jump_height := 1.2; # How high the player can jump under Earth's gravity
 const jump_time := 0.85; # How long a jump takes under Earth's gravity (assuming floor height is constant)
 const mouse_sensitivity = 0.01;
+const walk_speed = 5;
+const run_speed = 9;
 
 @onready var head = $Head;
 @onready var camera = $Head/Camera3D;
@@ -19,19 +21,26 @@ func _unhandled_input(event):
 		camera.rotate_x(-event.relative.y * mouse_sensitivity);
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90));
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _physics_process(delta):
+	if (Input.is_action_pressed("run")): speed = run_speed;
+	else: speed = walk_speed;
+	
 	if(not is_on_floor()): # gravity
 		velocity.y -= gravity * delta;
 		
 	elif (Input.is_action_pressed("jump")):
 		jump();
-	
+	# TODO this should be if on floor
 	var dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward");
-	velocity += (head.transform.basis * Vector3(dir.x, 0, dir.y)).normalized() * speed * delta; # update velocity based on speed and change in time, convert to vec3, then multiply by player rotation
+	var movVel = (head.transform.basis * Vector3(dir.x, 0, dir.y)).normalized()*speed; # update velocity based on speed and change in time, convert to vec3, then multiply by player rotation
+	var newVel = Vector3(velocity.x, 0, velocity.z).slerp(movVel, min(1, speed/(4*velocity.length()+0.05))); # rotate in motion direction. The faster you're going, the harder it is to change direction. Plus some number fiddling to get it to work with slerp (i.e. make it output in range 0-1)
+	velocity.x = newVel.x; # apply dir change
+	velocity.z = newVel.z; 
 	
-	if (dir == Vector2.ZERO and is_on_floor()):
-		velocity = Vector3(0, velocity.y, 0);
+	#if is_on_floor():# TODO implement floor friction coefficient. the default coeff is 0.9
+	#	velocity.x = sign(velocity.x)*max(abs(velocity.x) - 0.9*gravity*delta,0);
+	#	velocity.z = sign(velocity.z)*max(abs(velocity.z) - 0.9*gravity*delta,0);
 	
 	move_and_slide() # process velocity
 
